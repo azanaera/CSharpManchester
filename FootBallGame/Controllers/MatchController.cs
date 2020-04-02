60f870cbd52143506c24732b2f2a54873832810f
Era Azana <azanaera691@gmail.com>
2020-04-02 14:54:56 +0800

    WIP passing Team object to Index page

---
+++ FootBallGame/Controllers/MatchController.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using CsharpManchester;
+
+namespace FootBallGame.Controllers
+{
+    public class MatchController : Controller
+    {
+        private Team _team;
+        // GET: Match
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Match/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Match/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Match/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+                var calculatedMatches = new CalculatedMatches(collection["result"]);
+                ViewBag.Team = calculatedMatches.GetResults("Manchester United");
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Match/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Match/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Match/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Match/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
