1209cbe9afb4e309268b10b9acec4bf8960c5907
Era Azana <azanaera691@gmail.com>
2020-04-06 09:09:49 +0800

    Added ASP Core - no database

---
+++ FootBall/Controllers/HomeController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using FootBall.Models;
+
+namespace FootBall.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private static List<Team> _teams;
+        public IActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var team = _teams
+                .FirstOrDefault(m => m.Id == id);
+            if (team == null)
+            {
+                return NotFound();
+            }
+
+            return View(team);
+        }
+        [HttpPost]
+        public IActionResult CalculateMatch(string result)
+        {
+            var calculatedMatches = new CalculatedMatches(result);
+            _teams = calculatedMatches._teams;
+            return View(nameof(Index), _teams);
+        }
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult CalculateMatch()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ FootBall/Models/Team.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FootBall.Models
+{
+    public class Team
+    { 
+        private static int count;
+        public Team()
+        {
+            count++;
+            Id = count;
+        }
+        public int Id { get; }
+        public string Name { get; set; }
+        public int Wins { get; set; }
+        public int Losses { get; set; }
+        public int Draws { get; set; }
+        public int GamesPlayed { get; set; }
+        public int GoalsScored { get; set; }
+        public int GoalsConceded { get; set; }
+        public int TotalPoints { get =>  (Wins * 3) + Draws; }
+    }
+}
